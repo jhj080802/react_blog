bc19484634854da59886508ae91652e5b953be33
hyojoo <jhj080802@naver.com>
2024-08-04 16:21:12 +0900

    useState js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from "./logo.svg";
+import React from "react";
+// import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let posts = "강남 고기 맛집";
+  let [title, setTitle] = useState(["남자 코트 추천", "강남 우동 맛집"]);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={{ color: "blue", fontSize: "30px" }}>개발 blog</div>
+        <div>개발 Blog</div>
       </div>
-      <h4>{posts}</h4>
+      <div className="list">
+        <h3> {title} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
