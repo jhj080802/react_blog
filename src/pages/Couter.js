98c8b672e6da96cb6b09dcece103e634ce47d732
hyojoo <jhj080802@naver.com>
2024-08-04 17:54:34 +0900

    counter js

---
+++ src/pages/Couter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [num, setNum] = useState(0);
+
+  const increase = () => {
+    setNum(num + 1);
+  };
+
+  const decrease = () => {
+    setNum(num - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={increase}>+1</button>
+      <button onClick={decrease}>-1</button>
+      <p>{num}</p>
+    </div>
+  );
+}
