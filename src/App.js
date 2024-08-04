6c6771c36c963204eaa0ea6f45193e6f6cf5763a
hyojoo <jhj080802@naver.com>
2024-08-04 17:54:21 +0900

    move

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./page/home";
-import About from "./page/About";
+import Home from "./pages/home";
+import About from "./pages/About";
+import Counter from "./pages/Couter";
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Counter to="/counter">Counter</Counter>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
       </Routes>
     </div>
   );
