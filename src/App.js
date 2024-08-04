a1a0f2f176cfd1f99886a5921a6f58d20e610d8a
hyojoo <jhj080802@naver.com>
2024-08-04 16:41:30 +0900

    useState js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+/*eslint-disable*/
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [title, setTitle] = useState(["남자 코트 추천", "강남 우동 맛집"]);
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "자바 독학",
+  ]);
+  let [heart, setHeart] = useState(0);
 
   return (
     <div className="App">
@@ -12,8 +18,28 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> {title} </h3>
+        <h3>
+          {title[0]}
+          <span
+            onClick={() => {
+              setHeart(heart + 1);
+            }}
+          >
+            👍
+          </span>
+          {heart}
+        </h3>
         <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {title[1]} </h3>
+        <p>2월 18일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {title[2]} </h3>
+        <p>2월 19일 발행</p>
         <hr />
       </div>
     </div>
