d83c18859482c92a95c0cd26f19479a30acd3d13
hyojoo <jhj080802@naver.com>
2024-08-04 17:33:06 +0900

    clear

--- src/App.js
+++ src/App.js
@@ -1,59 +1,7 @@
-/*eslint-disable*/
 import React from "react";
-// import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
-import Modal from "./page/modal";
 
 function App() {
-  let [title, setTitle] = useState([
-    "남자 코트 추천",
-    "강남 우동 맛집",
-    "자바 독학",
-  ]);
-  let [heart, setHeart] = useState(0);
-
-  function changeTitle() {
-    var newArray = [...title];
-    newArray[0] = "여자 코트 추천";
-    setTitle(newArray);
-  }
-
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <div>개발 Blog</div>
-      </div>
-      <button onClick={changeTitle}>button</button>
-      <div className="list">
-        <h3>
-          {title[0]}
-          <span
-            onClick={() => {
-              setHeart(heart + 1);
-            }}
-          >
-            👍
-          </span>
-          {heart}
-        </h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3> {title[1]} </h3>
-        <p>2월 18일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3> {title[2]} </h3>
-        <p>2월 19일 발행</p>
-        <hr />
-      </div>
-
-      <Modal />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
