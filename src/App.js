04130692e695fd1bd1151c970574a7ccc00f169e
hyojoo <jhj080802@naver.com>
2024-08-04 17:45:29 +0900

    router - Routes, Route, Link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./page/home";
+import About from "./page/About";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
