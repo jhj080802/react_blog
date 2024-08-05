063d107b36074aef5e111c9243b69bebc1f25246
hyojoo <jhj080802@naver.com>
2024-08-05 12:39:19 +0900

    자동으로 생성 된거 지움

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |
         <Counter to="/counter">Counter</Counter>
       </nav>
       <Routes>
