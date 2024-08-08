e21407fdcb35fbfcda496f60a67c891359a5f936
hyojoo <jhj080802@naver.com>
2024-08-08 23:10:57 +0900

    add list

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/About";
 import Counter from "./pages/Couter";
+import List from "./pages/List";
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |
         <Link to="/list">List</Link>
         <Counter to="/counter">Counter</Counter>
       </nav>
@@ -16,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="/list" element={}/>
+        <Route path="/list" element={<List />} />
       </Routes>
     </div>
   );
