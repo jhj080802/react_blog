6cf3668907917aac68b9a720f638bce5e221d917
hyojoo <jhj080802@naver.com>
2024-08-12 11:34:47 +0900

    add input

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Home from "./pages/home";
 import About from "./pages/About";
 import Counter from "./pages/Couter";
 import List from "./pages/List";
+import Input from "./pages/Input";
 
 function App() {
   return (
     <div className="App">
       <nav>
         <Link to="/">Home</Link> | <Link to="/about">About</Link> |
-        <Link to="/list">List</Link>
-        <Counter to="/counter">Counter</Counter>
+        <Link to="/list">List</Link>///
+        <Link to="/counter">Counter</Link>
+        <Link to="/input">Input</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/input" element={<Input />} />
+
         <Route path="/list" element={<List />} />
       </Routes>
     </div>
---
+++ src/pages/Input.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function Input() {
+  const [txtValue, setTxtValue] = useState("");
+
+  const onChange = (e) => {
+    setTxtValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value="" onChange={onChange} />
+      <br />
+      <p>{txtValue}</p>
+    </div>
+  );
+}
