97e0d764dfbb32775e631eca8e53948cd1d88688
hyojoo <jhj080802@naver.com>
2024-08-12 15:57:56 +0900

    list

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,9 +1,34 @@
 import React from "react";
 
-export default function List() {
+export default function UserList() {
+  const User = ({ userData }) => {
+    return (
+      <tr>
+        <td>{userData.name}</td>
+        <td>{userData.email}</td>
+      </tr>
+    );
+  };
+
+  const users = [
+    { email: "h@gmail.com", name: "정효주" },
+    { email: "y@gmail.com", name: "주여진" },
+    { email: "s@gmail.com", name: "이서희" },
+    { email: "e@gmail.com", name: "유은서" },
+  ];
   return (
-    <div>
-      <div>List</div>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>이름</th>
+          <th>이메일</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <User userData={user} />
+        ))}
+      </tbody>
+    </table>
   );
 }
